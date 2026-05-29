3350c05b4846747b0d55b99994122412de5fac19
uripper <kripper11@gmail.com>
2026-05-29 17:44:17 -0600

    feat: update Windows download URL to version 0.1.1-alpha

--- apps/website/src/content/site-urls.ts
+++ apps/website/src/content/site-urls.ts
@@ -8,4 +8,4 @@ export const FEATURES_SECTION_URL = `${HOME_PAGE_URL}#features`;
 export const WORKFLOW_SECTION_URL = `${HOME_PAGE_URL}#workflow`;
 export const DOWNLOAD_SECTION_URL = "#download";
 export const WINDOWS_DOWNLOAD_URL =
-    "https://github.com/uripper/Bartleby/releases/download/Windows-v.0.1.0-alpha/Bartleby.Setup.0.1.0.exe";
+    "https://github.com/uripper/Bartleby/releases/download/Windows-v.0.1.1-alpha/Bartleby.Setup.0.1.1.exe";
